03126faa629a60cde4716744d48f635c98aac69d
Jinzheng Zhang <tianchaijz@gmail.com>
2016-10-22 13:34:43 +0800

    Initial commit

---
+++ src/interpreter.lua
@@ -0,0 +1,205 @@
+local os = require "os"
+local parser = require "parser"
+
+
+local type = type
+local error = error
+local pcall = pcall
+local print = print
+local pairs = pairs
+local ipairs = ipairs
+local setmetatable = setmetatable
+local unpack = unpack or table.unpack
+local format = string.format
+
+
+local function is_func(v) return type(v) == "function" end
+local function is_num(v) return type(v) == "number" end
+local function is_str(v) return type(v) == "string" end
+local function is_tbl(v) return type(v) == "table" end
+
+
+local function copytable(obj)
+    local t = {}
+    for k, v in pairs(obj) do
+        t[k] = is_tbl(v) and copytable(v) or v
+    end
+
+    return t
+end
+
+
+local RETURN   = 0
+local CONTINUE = 1
+local BREAK    = 2
+
+
+local function throw(kind, value)
+    error({ kind = kind, value = value })
+end
+
+
+local I = {}
+local mt = { __index = I }
+
+
+local _parent = -1
+local global_env = {
+    print = print,
+    exit = os.exit
+}
+
+
+local function resolve(id, env)
+    if not env then
+        return nil
+    end
+
+    return env[id] or resolve(id, env[_parent])
+end
+
+
+function I.new(env)
+    return setmetatable({ env = env or copytable(global_env) }, mt)
+end
+
+
+function I:_expr(node, env)
+    local tag = node[1]
+    if tag == "number" or tag == "string" then
+        return node[2]
+    end
+
+    if tag == "ref" then
+        local id = node[2]
+        return resolve(id, env) or
+            error(format("use of undefined identifier '%s'", id))
+    end
+
+    if tag == "call" then
+        return self:_call(node, env)
+    end
+
+    local op = tag
+    local lhs, rhs = self:_expr(node[2], env), self:_expr(node[3], env)
+    if op == "+" then
+        return lhs + rhs
+    elseif op == "-" then
+        return lhs - rhs
+    elseif op == "*" then
+        return lhs * rhs
+    elseif op == "/" then
+        return lhs / rhs
+    elseif op == ">" then
+        return lhs > rhs
+    elseif op == ">=" then
+        return lhs >= rhs
+    elseif op == "<" then
+        return lhs < rhs
+    elseif op == "<=" then
+        return lhs <= rhs
+    elseif op == "==" then
+        return lhs == rhs
+    elseif op == "!=" then
+        return lhs ~= rhs
+    end
+
+    error(format("unexpected tag '%s'", tag))
+end
+
+
+function I:_if(node, env)
+    local cond = self:_expr(node[2], env)
+    local else_node = node[4]
+    if cond then
+        return self:interpret(node[3], env)
+    elseif else_node then
+        return self:interpret(else_node[2], env)
+    end
+end
+
+
+function I:_while(node, env)
+    local cond = self:_expr(node[2], env)
+    while cond do
+        self:interpret(node[3], env)
+        cond = self:_expr(node[2], env)
+    end
+end
+
+
+function I:_def(node, env)
+    local id = node[2]
+    local pars = node[3]
+    local body = node[4]
+    env[id] = function(...)
+        local args = { ... }
+        local env = { [_parent] = env }
+        for k, p in ipairs(pars) do
+            env[p] = args[k]
+        end
+
+        return self:interpret(body, env)
+    end
+end
+
+
+function I:_call(node, env)
+    local id = node[2]
+    local func = resolve(id, env)
+    if not func then
+        error(format("use of undefined function '%s'", id))
+    end
+
+    local args = {}
+    for i, expr in ipairs(node[3]) do
+        args[i] = self:_exec(expr, env)
+    end
+
+    local ok, ret = pcall(function() return func(unpack(args)) end)
+    return ret and ret.value or nil
+end
+
+
+function I:_return(node, env)
+    local ret = node[2] and self:_expr(node[2], env) or nil
+    throw(RETURN, ret)
+end
+
+
+function I:_assign(node, env)
+    local id = node[2]
+    local expr = node[3]
+    local value = self:_exec(expr, env)
+
+    while true do
+        if env[id] or env == self.env then
+            env[id] = value
+            break
+        end
+        env = env[_parent]
+    end
+end
+
+function I:_local(node, env)
+    local id = node[2]
+    local expr = node[3]
+    env[id] = self:_exec(expr, env)
+end
+
+
+function I:_exec(node, env)
+    local tag = node[1]
+    local _exec = self["_" .. tag] or self._expr
+    return _exec(self, node, env)
+end
+
+
+function I:interpret(ast, env)
+    for _, node in ipairs(ast) do
+        self:_exec(node, env or self.env)
+    end
+end
+
+
+return I
---
+++ src/parser.lua
@@ -0,0 +1,147 @@
+local lpeg = require "lpeg"
+
+
+local type = type
+local tonumber = tonumber
+local match = lpeg.match
+
+
+lpeg.locale(lpeg)
+lpeg.setmaxstack(10000)
+
+
+local R, S, V, P, C, Ct, Cp, Cg, Cb, Cc, Cf =
+    lpeg.R, lpeg.S, lpeg.V, lpeg.P, lpeg.C, lpeg.Ct,
+    lpeg.Cp, lpeg.Cg, lpeg.Cb, lpeg.Cc, lpeg.Cf
+
+
+local Break = P"\r"^-1 * P"\n"
+local Comment = P("#") * (1 - P"\n")^0
+local White = S" \t\r\n"
+local Space = (White + Comment)^0
+local Alpha = lpeg.alpha
+local AlphaNum = R("az", "AZ", "09", "__")
+
+
+local mark = function(name)
+  return function(...)
+    return {
+      name,
+      ...
+    }
+  end
+end
+
+
+local remark = function(name)
+  return function(cap)
+    cap[1] = name
+    return cap
+  end
+end
+
+
+local pos = function(patt)
+  return (Cp() * patt) / function(pos, value)
+    if type(value) == "table" then
+      value[-1] = pos
+    end
+    return value
+  end
+end
+
+
+local keyword = function(chars)
+    return Space * chars * -AlphaNum
+end
+
+
+local sym = function(chars)
+  return Space * chars
+end
+
+
+local op = function(chars)
+    return Space * C(chars)
+end
+
+
+local function binaryOp(lhs, op, rhs)
+  if not op then
+    return lhs
+  end
+
+  return { op, lhs, rhs }
+end
+
+
+local function sepBy(patt, sep)
+    return patt * Cg(sep * patt)^0
+end
+
+
+local function chainOp(patt, sep)
+    return Cf(sepBy(patt, sep), binaryOp)
+end
+
+
+local function chain(patt, sep)
+    return patt * (sep * patt)^0
+end
+
+
+local Keywords = P"if" + P"else" + P"end" + P"def" + P"while" +
+                 P"return" + P"break" + P"continue"
+local Id = Space * (C((Alpha + P"_") * AlphaNum^0) - Keywords)
+local Num = Space * (P"-"^-1 * R"09"^1 / tonumber) / mark("number")
+local String = Space * (P'"' * C(((P"\\" * P(1)) + (P(1) - P'"'))^0) * P'"' +
+                        P"'" * C(((P"\\" * P(1)) + (P(1) - P"'"))^0) * P"'")
+                     / mark("string")
+
+local CompOp = op(S"><" * P"="^-1) + op(S"!=" * P"=")
+local AddOp = op(S"+-")
+local MulOp = op(S"*/")
+
+local program = P({
+    "Program",
+
+    Program = V"Block" + Ct"",
+    Block = Ct(V"Stat"^0),
+
+    Stat = V"Def" + V"Assign" + V"LocalAssign" + V"Return"
+         + V"Call" + V"If" + V"While",
+
+    Assign = Id * sym("=") * V"Expr" / mark("assign"),
+    LocalAssign = keyword("local") * V"Assign" / remark("local"),
+    Return = keyword("return") * (V"Expr" + Cc(nil)) / mark("return"),
+
+    CompExpr = V"Expr" * CompOp * V"Expr" / binaryOp,
+    Cond = V"CompExpr" + sym("(") * V"CompExpr" * sym(")") + V"Expr",
+
+    Expr = V"AddExpr",
+    AddExpr = chainOp(V"MulExpr", AddOp),
+    MulExpr = chainOp(V"Value", MulOp),
+    Ref = Id / mark("ref"),
+    Value = Num + String + V"Call" + V"Ref" + sym("(") * V"Expr" * sym(")"),
+
+    ParsList = Ct(chain(Id, sym(","))^0),
+    Pars = sym("(") * V"ParsList" * sym(")"),
+    Def = keyword("def") * Id * V"Pars" * V"Block" * keyword("end") / mark("def"),
+
+    ArgsList = Ct(chain(V"Expr", sym(","))^0),
+    Args = sym("(") * V"ArgsList" * sym(")"),
+    Call = Id * V"Args" / mark("call"),
+
+    IfElse = keyword("else") * V"Block" / mark("else"),
+    If = keyword("if") * V"Cond" * V"Block" * V"IfElse"^-1 * keyword("end") / mark("if"),
+
+    While = keyword("while") * V"Cond" * V"Block" * keyword("end") / mark("while"),
+})
+
+
+local function parse(source)
+    return match(program, source)
+end
+
+
+return { parse = parse }
